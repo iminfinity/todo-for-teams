ee4a8e5a80bd7ffc54b9c55755d65b4b3f87aa6f
iminfinity <this.is.me.infinity@gmail.com>
2020-08-07 18:46:09 +0530

    fixed createdAt  issue

--- models/todo.model.js
+++ models/todo.model.js
@@ -8,7 +8,7 @@ const Schema = mongoose.Schema;
 const todoSchema = new Schema(
   {
     task: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now() },
+    createdAt: { type: Date, default: Date.now },
   },
   {
     timestamps: true,
